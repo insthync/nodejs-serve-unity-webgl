2dd01e1228803c9ec291001dcde3498e7e9bf878
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-06-13 07:44:25 +0700

    Add compression format setting

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const https = require('https');
 const http = require('http');
 require('dotenv').config();
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(express.static('public'))
-
 const port = Number(process.env.SERVER_PORT || 8000);
 const useHttps = Number(process.env.USE_HTTPS || 0) > 0;
 const keyFilePath = process.env.HTTPS_KEY_FILE_PATH;
 const certFilePath = process.env.HTTPS_CERT_FILE_PATH;
 const httpsPort = Number(process.env.HTTPS_SERVER_PORT || 8080);
+const compressionFormat = process.env.COMPRESSION_FORMAT || '';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public', {
+    setHeaders: function(res, path) {
+        if(path.endsWith(".unityweb")) {
+            if (compressionFormat == "gzip" || compressionFormat == "br") {
+                res.set("Content-Encoding", compressionFormat);
+            }
+        }
+    }
+}));
 
 const httpServer = http.createServer(app);
 httpServer.listen(port, () => {
