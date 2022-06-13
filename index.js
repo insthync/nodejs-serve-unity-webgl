b112137a6b20354f9fb0ed4e0d81fc66f572d5c0
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-06-13 08:29:50 +0700

    Change compression handling

--- index.js
+++ index.js
@@ -9,17 +9,18 @@ const useHttps = Number(process.env.USE_HTTPS || 0) > 0;
 const keyFilePath = process.env.HTTPS_KEY_FILE_PATH;
 const certFilePath = process.env.HTTPS_CERT_FILE_PATH;
 const httpsPort = Number(process.env.HTTPS_SERVER_PORT || 8080);
-const compressionFormat = process.env.COMPRESSION_FORMAT || '';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public', {
     setHeaders: function(res, path) {
-        if(path.endsWith(".unityweb")) {
-            if (compressionFormat == "gzip" || compressionFormat == "br") {
-                res.set("Content-Encoding", compressionFormat);
-            }
+        if (path.endsWith(".data.gz") || path.endsWith(".framework.js.gz")) {
+            res.set("Content-Encoding", "gzip");
+        }
+        if (path.endsWith(".wasm.gz")) {
+            res.set("Content-Type", "application/wasm");
+            res.set("Content-Encoding", "gzip");
         }
     }
 }));
